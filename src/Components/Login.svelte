605053e0c44e5005d6b7bc43dbe4ba2da6893ee7
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2024-03-13 20:51:36 -0400

    got the basic of the welcome page

--- src/Components/Login.svelte
+++ src/Components/Login.svelte
@@ -2,55 +2,73 @@
   let username = '';
   let password = '';
   let state = '';
-
+  let isLoggedIn = false;
   let isLogin = true; // Determines whether the user is in the login or signup mode
-
+  
   const handleLogin = () => {
     // Perform login logic here
     console.log('Logging in with:', username, password);
-    // You should only have login logic here, not other HTML or Svelte syntax
+    isLoggedIn = true;
   };
-
+  
   const handleSignup = () => {
     // Perform signup logic here
     console.log('Signing up with:', username, password, state);
-    // Similar to login, only signup logic should be here
+    isLoggedIn = true;
   };
-
+  
   const toggleMode = () => {
     isLogin = !isLogin;
   };
+
+  const handleLogout = () => {
+    isLoggedIn = false;
+    username = '';
+    password = '';
+    state = '';
+  };
 </script>
-
-{#if isLogin}
-  <h2>Login</h2>
+  
+{#if isLoggedIn}
+  <h2>Welcome, {username}!</h2>
+  <button type="button" on:click={handleLogout}>Sign Out</button>
 {:else}
-  <h2>Sign Up</h2>
-{/if}
-
-<form on:submit|preventDefault>
-  <label>
-    Username:
-    <input type="text" bind:value={username} />
-  </label>
-
-  <label>
-    Password:
-    <input type="password" bind:value={password} />
-  </label>
-
-  {#if !isLogin}
-    <label>
-      State:
-      <input type="text" bind:value={state} />
-    </label>
-  {/if}
-
   {#if isLogin}
-    <button type="button" on:click={handleLogin}>Login</button>
-    <p>Don't have an account? <a href="#/" on:click|preventDefault={toggleMode}>Sign up here</a>.</p>
+    <h2>Login</h2>
+    <form on:submit|preventDefault>
+      <label>
+        Username:
+        <input type="text" bind:value={username} />
+      </label>
+    
+      <label>
+        Password:
+        <input type="password" bind:value={password} />
+      </label>
+    
+      <button type="button" on:click={handleLogin}>Login</button>
+      <p>Don't have an account? Sign up <a href="#/" on:click|preventDefault={toggleMode}>here</a>.</p>
+    </form>
   {:else}
-    <button type="button" on:click={handleSignup}>Sign Up</button>
-    <p>Already have an account? <a href="#/" on:click|preventDefault={toggleMode}>Login here</a>.</p>
+    <h2>Sign Up</h2>
+    <form on:submit|preventDefault>
+      <label>
+        Username:
+        <input type="text" bind:value={username} />
+      </label>
+    
+      <label>
+        Password:
+        <input type="password" bind:value={password} />
+      </label>
+    
+      <label>
+        State:
+        <input type="text" bind:value={state} />
+      </label>
+    
+      <button type="button" on:click={handleSignup}>Sign Up</button>
+      <p>Already have an account? Login <a href="#/" on:click|preventDefault={toggleMode}>here</a>.</p>
+    </form>
   {/if}
-</form>
+{/if}
