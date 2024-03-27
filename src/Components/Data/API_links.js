4fec663cb395a60b35389e0bb911f51e783d6281
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2024-03-27 16:19:14 -0400

    changed api links

---
+++ src/Components/Data/API_links.js
@@ -0,0 +1,24 @@
+const apiKey = process.env.API_KEY;
+
+const urls = [
+    `https://api.congress.gov/v3/bill/117/hr/3076?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/100/sjres/114?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/102/hconres/95?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/108/hr/2551?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/107/hr/4942?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/111/hconres/122?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/105/hr/2534?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/118/hr/1915?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/118/hr/2519?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/111/hr/4692?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/112/s/1399?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/106/hr/649?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/100/s/1310?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/104/hr/2421?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/106/hr/2999?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/113/hr/916?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/100/hjres/529?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/104/s/1781?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/108/sconres/35?api_key=${apiKey}`,
+    `https://api.congress.gov/v3/bill/105/s/1744?api_key=${apiKey}`
+];
